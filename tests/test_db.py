4cc3df2eb9ab417602d8c6a9e8feb5a8532d87fd
Matt Kramer <matthew.robert.kramer@gmail.com>
2021-07-24 22:33:29 -0500

    add: test for random assignment of "X" and "O"

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,9 +1,11 @@
 from typing import Generator
+from typing import List
 
 import pytest
 
 from tic_tac_toe.db import Game
 from tic_tac_toe.db import NULL_CHAR
+from tic_tac_toe.db import Player
 
 
 @pytest.fixture()
@@ -21,6 +23,26 @@ def test_save_game(game: Game) -> None:
     assert game.state == NULL_CHAR * 9
     assert game.player_x is not None
     assert game.player_o is not None
+
+
+@pytest.fixture()
+def games() -> Generator[List[Game], None, None]:
+    """Create multiple games with the same two players, randomly assigned to "X" and "O"."""
+    games = []
+    for _ in range(10):
+        game = Game(players=["Matt", "Bob"])
+        game.save()
+        games.append(game)
+    yield games
+    for game in games:
+        game.delete()
+
+
+def test_assign_players_randomly(games: List[Game]) -> None:
+    """Given enough games, both players should be assigned to "X" at least once."""
+    player_x_names = {game.player_x.name for game in games}
+    assert len(player_x_names) == 2
+    assert Player.query.count() == 2
 
 
 def test_delete_game(game: Game) -> None:
